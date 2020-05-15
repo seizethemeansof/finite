c7068a1282e4ea50877c205f184400ec9b473b5e
seizethemeansof <samy.clinchard@gmail.com>
2020-05-15 14:02:30 +0300

    Generate new on click

--- audio.js
+++ audio.js
@@ -1,6 +1,3 @@
-let pentatonic_scale_bass = [["C2", "D2", "E2", "G2", "A2", "C3", "r"]];
-let pentatonic_scale_aigu = ["C3", "D3", "E3", "G3", "A3", "C4", "r"];
-
 let all_notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 
 function createDuration(n_measures) {
@@ -49,7 +46,13 @@ function chooseNote(chords_progression, duration) {
 function chooseChords(key, len) {
   // Return a set of length len of elements belonging to scale
   chords = [];
-  key_chords = Tonal.Key.majorKey(key).chords
+  if (Math.round(Math.random()) == 1) {
+    key_chords = Tonal.Key.majorKey(key).chords;
+    console.log('Major');
+  } else {
+    key_chords = Tonal.Key.minorKey(key).natural.chords;
+    console.log('Minor');
+  }
   for (var i = 0; i < len; i++) {
     chords.push(random(key_chords));
   }
@@ -92,70 +95,62 @@ function createChordsPhrase(duration, time, note) {
   return phrase;
 }
 
-function ChordsInstruments(chords_progression) {
-  this.chords_progression = chords_progression
-  // Set the instrument
-  this.sound = new Tone.PolySynth(3, Tone.Synth, {
-    oscillator : {
-      type : "sine"
-    }
-  }).toMaster();
-  this.sound.volume.value = -12;
-  // Create a phrase for the instrument
-  this.durations = new Array(this.chords_progression.length);
-  this.durations.fill(1);
-  this.durations = [this.durations];
-  this.times = durationToTime(this.durations);
-  this.notes = this.chords_progression;
-  this.phrase = createChordsPhrase(this.durations[0], this.times, this.notes);
-  // Create the part
-  this.part = new Tone.Part((time, value) => {
-    this.sound.triggerAttackRelease(value.note, value.duration, time);
-  }, this.phrase);
-  this.part.start(0);
-  this.part.loopEnd = this.chords_progression.length + 'm'
-  this.part.loop = true;
-}
-
-ChordsInstruments.prototype.generateNew = function () {
-  this.part.removeAll()
-  // Create a phrase for the instrument
-  this.durations = createDuration(this.n_measures);
-  this.times = durationToTime(this.durations);
-  this.notes = chooseNote(this.scale, this.times.length);
-  this.phrase = createPhrase(this.durations, this.times, this.notes);
-  // Create the part
-  this.part = new Tone.Part((time, value) => {
-    this.sound.triggerAttackRelease(value.note, value.duration, time);
-  }, this.phrase);
-  this.part.start(0);
-  this.part.loopEnd = this.n_measures + 'm'
-  this.part.loop = true;
-}
-
-function Instrument(chords_progression, octave) {
-  this.octave = octave
-  // Set the instrument
-  this.sound = new Tone.Synth({
-    oscillator : {
-      type : "sine"
-    }
-  }
-    
-  ).toMaster();
-  // Create a phrase for the instrument
+function Instrument(chords_progression, octave, poly) {
+  this.poly = poly;
+  this.octave = octave;
   this.chords_progression = chords_progression;
-  this.durations = createDuration(this.chords_progression.length);
-  this.times = durationToTime(this.durations);
-  this.notes = chooseNote(this.chords_progression, this.durations);
-  this.phrase = createPhrase(this.durations, this.times, this.notes, this.octave);
-  // Create the part
-  this.part = new Tone.Part((time, value) => {
-    this.sound.triggerAttackRelease(value.note, value.duration, time);
-  }, this.phrase);
-  this.part.start(0);
-  this.part.loopEnd = this.chords_progression.length + 'm'
-  this.part.loop = loop;
+  // Set the instrument
+  if (poly) {
+    // If poly is true, it will create a polyphonic instrument
+    this.sound = new Tone.PolySynth(3, Tone.Synth, {
+      oscillator: {
+        type: "sine"
+      }
+    }).toMaster();
+    this.sound.volume.value = -12;
+  } else {
+    this.sound = new Tone.Synth({
+      oscillator: {
+        type: "sine"
+      }
+    }
+    ).toMaster();
+  }
+  this.part = new Tone.Part();
+  this.generate();
+}
+
+Instrument.prototype.generate = function () {
+  this.part.removeAll()
+  if (this.poly) {
+    // Create a phrase for the instrument
+    this.durations = new Array(this.chords_progression.length);
+    this.durations.fill(1);
+    this.durations = [this.durations];
+    this.times = durationToTime(this.durations);
+    this.notes = this.chords_progression;
+    this.phrase = createChordsPhrase(this.durations[0], this.times, this.notes);
+    // Create the part
+    this.part = new Tone.Part((time, value) => {
+      this.sound.triggerAttackRelease(value.note, value.duration, time);
+    }, this.phrase);
+    this.part.start(0);
+    this.part.loopEnd = this.chords_progression.length + 'm';
+    this.part.loop = true;
+  } else {
+    // Create a phrase for the instrument
+    this.durations = createDuration(this.chords_progression.length);
+    this.times = durationToTime(this.durations);
+    this.notes = chooseNote(this.chords_progression, this.durations);
+    this.phrase = createPhrase(this.durations, this.times, this.notes, this.octave);
+    // Create the part
+    this.part = new Tone.Part((time, value) => {
+      this.sound.triggerAttackRelease(value.note, value.duration, time);
+    }, this.phrase);
+    this.part.start(0);
+    this.part.loopEnd = this.chords_progression.length + 'm';
+    this.part.loop = true;
+  }
 }
 
 Instrument.prototype.muteOrUnmute = function () {
@@ -167,22 +162,6 @@ Instrument.prototype.muteOrUnmute = function () {
     this.part.mute = true;
     this.muteButton.innerHTML = "Unmute";
   }
-}
-
-Instrument.prototype.generateNew = function () {
-  this.part.removeAll()
-  // Create a phrase for the instrument
-  this.durations = createDuration(this.n_measures);
-  this.times = durationToTime(this.durations);
-  this.notes = chooseNote(this.scale, this.times.length);
-  this.phrase = createPhrase(this.durations, this.times, this.notes);
-  // Create the part
-  this.part = new Tone.Part((time, value) => {
-    this.sound.triggerAttackRelease(value.note, value.duration, time);
-  }, this.phrase);
-  this.part.start(0);
-  this.part.loopEnd = this.n_measures + 'm'
-  this.part.loop = true;
 }
 
 // Callback to play
@@ -221,11 +200,12 @@ function stopMusic() {
 function generateMusic() {
   Tone.Transport.stop();
   key = random(all_notes);
-  for (j = 0; j < instruments.length; j++) {
-    instruments[j].scale = Tonal.Scale.get(key + (j+2) + " pentatonic").notes;
-    instruments[j].generateNew();
+  chords_progression = chooseChords(key, 4);
+  for (var i = 0; i < instruments.length; i++) {
+    instruments[i].chords_progression = chords_progression;
+    instruments[i].generate();
   }
-  Tone.Transport.bpm.value = random(40, 80);
+  Tone.Transport.bpm.value = Math.round(random(40, 90));
   Tone.Transport.start();
 }
 
@@ -235,7 +215,7 @@ let key;
 function setup() {
   Tone.Transport.loopEnd = '4m';
   Tone.Transport.loop = true;
-  Tone.Transport.bpm.value = random(40, 80);  
+  Tone.Transport.bpm.value = Math.round(random(40, 90));
 
   // Get the key
   key = random(all_notes);
@@ -243,16 +223,17 @@ function setup() {
   chords_progression = chooseChords(key, 4);
 
   // Chords section
-  back_chords = new ChordsInstruments(chords_progression, 4);
+  chords = new Instrument(chords_progression, "3", true);
+  instruments.push(chords)
 
   // Bass section
-  bass = new Instrument(chords_progression, "2");
+  bass = new Instrument(chords_progression, "2", false);
   instruments.push(bass);
 
   // Melody section
-  melody = new Instrument(chords_progression, "4");
+  melody = new Instrument(chords_progression, "4", false);
   instruments.push(melody);
 
-  
+
 }
 
